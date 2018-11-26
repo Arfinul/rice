15718ca30fdc8331e35db8391da981f65e5d3672
Md Arfinul Haque <mdarfinulhaque@gmail.com>
2018-11-27 01:34:13 +0530

    fix - value and image

--- server.py
+++ server.py
@@ -4,7 +4,8 @@ import os
 
 # Initialize the Flask application
 app = Flask(__name__)
-cwd = '/home/agnexttech/trap_next/images'
+# cwd = '/home/agnexttech/trap_next/images'
+cwd = 'C:\\Users\Agnext-Arfin\Desktop\\uploaded'
 
 
 # Route http posts to this method
@@ -14,15 +15,16 @@ def test():
     print('Uploading the file ... Wait !!!')
 
     file = request.files['image']
+    val = request.form['text']
     file.save(file.filename)
     # print(file.filename)
     print('Uploaded - ', file.filename)
-
-    response = {'Image Uploaded'
+    response = {'Image Uploaded', val
                 }
     response_pickled = jsonpickle.encode(response)
     return Response(response=response_pickled, status=200, mimetype="application/json")
 
 
 # start flask app
-app.run(host="0.0.0.0", port=5001)
+app.run(port=4002)
+# app.run(host="0.0.0.0", port=5001)
